a7021edf3e2e4d5694f878c900f098309e380a5b
Adetayor <adetayo456@gmail.com>
2024-12-05 03:23:50 +0100

    typeScript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// console. log('Hello World');
+// let age: number = 20;
+// if (age < 50)
+//   age += 10;
+// console. log(age);
+// let sales: number = 123_456_789; 
+// let course: string = 'TypeScript';
+// let is_published: boolean = true;
+let sales = 123_456_789; 
+let course = 'TypeScript';
+let is_published = true;
+let level;
