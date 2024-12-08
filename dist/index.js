e11768c372cf198b64b32de6503370fc08ebabe6
Adetayor <adetayo456@gmail.com>
2024-12-08 04:54:14 +0100

    ts-array

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let user = [1, 'Mosh'];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,34 @@ let sales = 123_456_789;
 let course = 'TypeScript';
 let is_published = true;
 let level;
+// level = 1;
+// level = 'a';
+// function render(document) {
+//   console.log(document);
+// }
+function render(document: any) {
+  console.log(document);
+}
+//Array
+// let numbers = [1, 2, '3']; correct with Normal Js
+// let numbers: number[] = [1, 2, '3'];
+// let numbers = [1, 2, 3];
+// let numbers = []; //to use any empty array you have to apply a type anotation
+// numbers[0] = 1;
+// numbers[1] = '1';
+// let numbers: number[] = [];
+// numbers[0] = 1;
+// numbers[1] = '1';
+// let numbers: number[] = [];
+// numbers.forEach(n => n.)
+
+//Tuples
+// let user: [number, string] = [1, 'Mosh', 0];
+// let user: [number, string] = ['1', 'Mosh']; complilation error
+// let user: [number, string] = [1, 'Mosh'];
+// user[0].
+// user[1].
+// let user: [number, string] = [1, 'Mosh'];
+// user.push(1); //gap in typeScript
+let user: [number, string, boolean, number] = [1, 'Mosh', true, 0]; //tuples are most usefull with 2 values
+//Enums rep a list of related constants
