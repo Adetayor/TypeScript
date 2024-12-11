e47c6900642a7f478e487214732c2dcaceb882f4
Adetayor <adetayo456@gmail.com>
2024-12-11 04:22:35 +0100

    ts-functions

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,11 @@ let level;
 function render(document) {
     console.log(document);
 }
-let user = [1, 'Mosh'];
+let user = [1, 'Mosh', true, 0];
+const small = 1;
+const medium = 2;
+const large = 3;
+;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,84 @@ function render(document: any) {
 // let user: [number, string] = [1, 'Mosh'];
 // user.push(1); //gap in typeScript
 let user: [number, string, boolean, number] = [1, 'Mosh', true, 0]; //tuples are most usefull with 2 values
+
 //Enums rep a list of related constants
+//list of tshirt sizes
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// PascalCase Naming convention
+// enum Size { Small = 0, Medium = 1, Large = 2}; //typeScript default
+// enum Size {Small = 1, Medium = 2, Large = 3 }; //we set our own values
+// enum Size {Small = 's', Medium = 'm', Large = 'l' };
+// enum Size {Small = 1, Medium, Large}; 
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+// const enum Size {Small = 1, Medium, Large}; 
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+
+//functions
+// function calculateTax(income: number) {
+
+// }
+// function calculateTax(income: number) {
+//   return 0;
+// }
+// function calculateTax(income: number): void {
+  
+// }
+// function calculateTax(income: number): number {
+//   return 0;
+// }
+// function calculateTax(income: number): number { if (income < 50_000)
+//   return income * 1.2; // JavaScript always returns undefined function
+// undefined is not a number}
+// function calculateTax(income: number) { if (income < 50_000)
+//   return income * 1.2; // if the result is greater than 50_000, we are going to get undefined and this may cause a bug in our application
+// }
+// function calculateTax(income: number) { if (income < 50_000)  
+//  return income * 1.2;
+//  return income * 1.3;
+// }
+// function calculateTax(income: number): number { let x;
+//   if (income < 50_000)  
+//   return income * 1.2;
+//   return income * 1.3;
+//  }
+// function calculateTax(income: number, taxYear: number): number {
+//   if (taxYear < 2022)  
+//   return income * 1.2;
+//   return income * 1.3;
+//  }
+
+//  calculateTax(10_000, 2022);
+// function calculateTax(income: number, taxYear?: number): number {
+//   if (taxYear < 2022)  
+//   return income * 1.2;
+//   return income * 1.3;
+//  }
+
+//  calculateTax(10_000); //if the taxyear is not given, the result will be undefined
+// function calculateTax(income: number, taxYear?: number): number {
+//   if ((taxYear || 2022) < 2022)  
+//   return income * 1.2;
+//   return income * 1.3;
+//  }
+
+//  calculateTax(10_000);
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if ((taxYear || 2022) < 2022)  
+//   return income * 1.2;
+//   return income * 1.3;
+//  }
+
+//  calculateTax(10_000, 2023); //what is passed (2023) will over ride the default value 2022
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022)  
+  return income * 1.2;
+  return income * 1.3;
+ }
+
+ calculateTax(10_000);
