7632942301107ee877286b79597d5f9474d694ed
Adetayor <adetayo456@gmail.com>
2024-12-20 05:22:47 +0100

    typeScript-1

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,32 @@ let user = [1, 'Mosh', true, 0];
 const small = 1;
 const medium = 2;
 const large = 3;
-;
-let mySize = 2;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs('10kg');
+let weight;
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');
+}
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : {
+        birthday: new Date()
+    };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -114,10 +114,156 @@ const large = 3;
 //  }
 
 //  calculateTax(10_000, 2023); //what is passed (2023) will over ride the default value 2022
-function calculateTax(income: number, taxYear = 2022): number {
-  if (taxYear < 2022)  
-  return income * 1.2;
-  return income * 1.3;
- }
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2022)  
+//   return income * 1.2;
+//   return income * 1.3;
+//  }
 
- calculateTax(10_000);
+//  calculateTax(10_000);
+
+//Objects
+// let employee = { id: 1 };
+// employee.name = 'Mosh';
+// let employee: {
+//   id: number,
+//   name: string
+// } = { id: 1, name: 'Mosh' };
+// let employee: {
+//   readonly id: number,
+//   name: string
+// } = { id: 1, name: 'Mosh' };
+// employee.id = 0; // typeScript prevents us from acidentally modifying the value of dis property
+// let employee: {
+//   readonly id: number,
+//   name: string,
+//   retire: (date: Date) => void //the problem is if we want to create another employee object we have to repeat this structure & we dont want to repeat ourselves
+// } = { 
+//   id: 1, 
+//   name: 'Mosh',
+//   retire: (date: Date) => {
+//     console.log(date);
+//   }
+// };
+
+//Advanced Types - Type Aliases
+// type Employee = {
+//   readonly id: number,
+//   name: string,
+//   retire: (date: Date) => void
+// }
+
+// let employee: Employee = {
+// } = { 
+//   id: 1, 
+//   name: 'Mosh',
+//   retire: (date: Date) => {
+//     console.log(date);
+//   }
+// };
+
+//Union Types
+function kgToLbs(weight: number | string) {
+  //Narrowing
+  if (typeof weight === 'number')
+    return weight * 2.2;
+  else
+    return parseInt(weight) * 2.2;
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
+
+//Intersection Types
+let weight: number & string; // both a number and a string but not visible
+type Draggable = {
+   drag: () => void
+};
+
+type Resizeable = {
+  resize: () => void
+};
+
+type UIWidget = Draggable & Resizeable;
+
+let textBox: UIWidget ={
+  drag: () => {},
+  resize: () => {}
+}
+
+//Literal Types
+//anontate with a literal meaning and exact or specific value.  Literal (exact, specific)
+type Quantity= 50 | 100;
+let quantity: Quantity = 100;
+//literls can also be strings
+type Metric = 'cm' | 'inch';
+
+//Nullable Types
+// function greet(name: string | null) {
+//   if (name)
+//     console.log(name.toUpperCase());
+//   else
+//     console.log('Hola!');
+// }
+
+// greet(null);
+function greet(name: string | null | undefined) {
+  if (name)
+    console.log(name.toUpperCase());
+  else
+    console.log('Hola!');
+}
+
+greet(undefined);
+
+//Optional Chaining
+// type Customer ={
+//   birthday: Date
+// };
+
+// function getCustomer(id: number): Customer | null |undefined {
+//   return id === 0 ? null : {
+//     birthday: new Date() 
+//   };
+// }
+
+// let customer = getCustomer(0);
+// if (customer !== null && customer !== undefined)
+// console.log(customer.birthday);
+
+// type Customer ={
+//   birthday: Date
+// };
+
+// function getCustomer(id: number): Customer | null |undefined {
+//   return id === 0 ? null : {
+//     birthday: new Date() 
+//   };
+// }
+
+// let customer = getCustomer(1);
+// // Optional property access operator
+// console.log(customer?.birthday);
+
+type Customer ={
+  birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null |undefined {
+  return id === 0 ? null : {
+    birthday: new Date() 
+  };
+}
+
+let customer = getCustomer(1);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+//Optional element access operator
+//if (customers !== null && customers !== undefined)
+//customers?.[0]
+
+//Optional call
+// let log: any = (message: string) => console.log(message)
+let log: any = null;
+log?.('a');
